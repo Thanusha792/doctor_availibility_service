d876bde77272ea65a533e2f6467feca6ae553ba0
Thanusha792 <73287419+Thanusha792@users.noreply.github.com>
2022-03-16 13:10:36 +0530

    Update AvailibilityRepository.java

--- Doctor-availability-service/src/main/java/com/capgemini/DA/repository/AvailibilityRepository.java
+++ Doctor-availability-service/src/main/java/com/capgemini/DA/repository/AvailibilityRepository.java
@@ -1,4 +1,4 @@
-package com.capgemini.DA.repository;
+package com.cap.DA.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
